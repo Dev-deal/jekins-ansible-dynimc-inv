0246d6f779936820e5205987399a07eb24841236
Amit-Rikhari <116537890+Amit-Rikhari@users.noreply.github.com>
2022-12-20 14:18:52 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,13 +3,14 @@ provider "aws"{
 region = "us-east-1"
 }
 
-resource "aws_instance"{
+resource "aws_instance" "AWSServer"{
 ami = "ami-08e637cea2f053dfa"
 instance_type = "t2.micro"
 key_name = "Key_Ec2.pem"
 security_groups = ["launch-wizard-24"]
 
-tags = "tomcatservers"
+tags = {
+  Name= "tomcatservers"}
 
 
 }
