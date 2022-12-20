fae872e82147caad079885523d7ef9fd82fa3a0c
Amit-Rikhari <116537890+Amit-Rikhari@users.noreply.github.com>
2022-12-20 13:53:48 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "AWSInstance"{
-ami = "ami-010d564ad44bd1912"
+ami = "ami-08e637cea2f053dfa"
 instance_type = "t2.micro"
 key_name = "Key_Ec2.pem"
 security_group = ["launch-wizard-24"]
