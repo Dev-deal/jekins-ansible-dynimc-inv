f5da0d1adc601f00b2498d5732590cf58dfe6ae6
Amit-Rikhari <116537890+Amit-Rikhari@users.noreply.github.com>
2022-12-20 14:21:18 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -6,7 +6,7 @@ region = "us-east-1"
 resource "aws_instance" "AWSServer"{
 ami = "ami-08e637cea2f053dfa"
 instance_type = "t2.micro"
-key_name = "Key_Ec2.pem"
+key_name = "Key_Ec2"
 security_groups = ["launch-wizard-24"]
 
 tags = {
